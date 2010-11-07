804502e833b3ae5a96cc4334d92b6105780ab329
Sean Wellington <sean@tinman.home>
2010-11-07 09:45:39 -0500

    Added LICENSE to jar file

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -2,6 +2,10 @@ import sbt._
 
 class Project(info:ProjectInfo) extends DefaultProject(info) {
 
+  def extraResources = "LICENSE"
+  
+  override def mainResources = super.mainResources +++ extraResources
+
   val servlet = "javax.servlet" % "servlet-api" % "2.5" % "provided"
 
   val scalatest = "org.scalatest" % "scalatest" % "1.2" % "test"
